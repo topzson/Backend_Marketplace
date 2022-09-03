9ff580ef47758739798262fc0c8e4dc88d7445bd
Rattatammanoon Prawasuk <toprattatammanoon@hotmail.com>
2022-09-03 21:41:13 +0700

    Base Database

---
+++ main.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"github.com/topzson/Backend_Marketplace/controller"
+
+	"github.com/topzson/Backend_Marketplace/entity"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+
+	entity.SetupDatabase()
+
+	r := gin.Default()
+
+	// User Routes
+
+	r.GET("/users", controller.ListUsers)
+
+	r.GET("/user/:id", controller.GetUser)
+
+	r.POST("/users", controller.CreateUser)
+
+	r.PATCH("/users", controller.UpdateUser)
+
+	r.DELETE("/users/:id", controller.DeleteUser)
+
+	// Run the server
+	r.Use(CORSMiddleware())
+	r.Run()
+
+}
+
+func CORSMiddleware() gin.HandlerFunc {
+
+	return func(c *gin.Context) {
+
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+
+		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT")
+
+		if c.Request.Method == "OPTIONS" {
+
+			c.AbortWithStatus(204)
+
+			return
+
+		}
+
+		c.Next()
+
+	}
+
+}
